a98233a4c5d8fb41e27135fdc7d229d529a9d41d
Je1ghtxyuN <3442408723@qq.com>
2025-03-09 13:54:49 +0800

    新增了一个测试场景
    没干别的

---
+++ Assets/脚本文件/ShuiLongTouInteraction.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShuiLongTouInteraction : MonoBehaviour
+{
+   
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
