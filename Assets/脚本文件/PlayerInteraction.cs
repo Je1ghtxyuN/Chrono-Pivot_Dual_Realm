b2a93d20e66b71ec81f4073488293f0886cd8958
Je1ghtxyuN <3442408723@qq.com>
2025-03-09 16:00:39 +0800

    新增了互动传送逻辑
    距离，高亮，扳机键的判断条件

--- Assets/脚本文件/PlayerInteraction.cs
+++ Assets/脚本文件/PlayerInteraction.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
 
-public class ShuiLongTouInteraction : MonoBehaviour
+public class PlayerInteraction : MonoBehaviour
 {
    
     void Start()
