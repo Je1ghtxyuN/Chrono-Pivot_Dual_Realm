8abd836e0f70154ae7ab30598b2d0ca199db87a1
Je1ghtxyuN <3442408723@qq.com>
2025-03-09 14:43:03 +0800

    把盐和日晷做成预制体
    添加了预制体文件夹

--- Assets/脚本文件/ShuiLongTouInteraction.cs
+++ Assets/脚本文件/ShuiLongTouInteraction.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
 
 public class ShuiLongTouInteraction : MonoBehaviour
 {
    
     void Start()
     {
-        
+       
     }
 
    
