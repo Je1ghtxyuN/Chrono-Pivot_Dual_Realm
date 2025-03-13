42722bd8cf872ea4c8f1552ba8ba964417b368e5
aaaaas811 <3196611630@qq.com>
2025-03-13 22:43:57 +0800

    更新 桥梁预制件
    顺便学习了一下粒子系统，做了个稍微通用些的触发短时间粒子的脚本

--- Assets/脚本：as811/BridgeController.cs
+++ Assets/脚本：as811/BridgeController.cs
@@ -1,22 +1,39 @@
 using UnityEngine;
 
+
 public class BridgeController : MonoBehaviour
 {
-    public Transform bridge;         
+    [Header("基础设置")]
+    public Transform bridge;
     public Transform arrow;
-   
     public float rotateSpeed = 10f; // 旋转平滑度
-
+    [Header("粒子系统设置")]
+    public ParticleController particleController;
+    //需要改旋转的目标角度时改这里
+    private Quaternion targetRot = Quaternion.LookRotation(Vector3.forward);
+    
+    
     private bool isTriggered = false;
     private Quaternion initialRotation;
-
+    //保护性，防止多次触发
+    private bool defend = false;
+    void Start()
+    {
+        particleController=GetComponent<ParticleController>();
+        if (bridge != null)
+        {
+            initialRotation = bridge.rotation;
+        }
+        else
+        {
+            Debug.LogError("哥们你桥呢？", this);
+        }
+    }
     void OnTriggerEnter(Collider other)
     {
         if (other.transform == arrow)
         {
             isTriggered = true;
-            
-            initialRotation = bridge.rotation;
             Debug.Log("Enter");
         }
     }
@@ -31,15 +48,22 @@ public class BridgeController : MonoBehaviour
 
     void Update()
     {
+        if (bridge.rotation == targetRot && !defend)
+        {
+            StartCoroutine(particleController.PlayAndStop());
+            defend = true;
+        }
         if (isTriggered)
         {
             // 旋转插值
-            Quaternion targetRot = Quaternion.LookRotation(Vector3.forward);
+            
             bridge.rotation = Quaternion.Slerp(
                 bridge.rotation,
                 targetRot,
                 Time.deltaTime * rotateSpeed
             );
+            
         }
+
     }
 }
---
+++ Assets/脚本：as811/ParticleController.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using System.Collections;
+
+public class ParticleController : MonoBehaviour
+{
+    [Header("基础设置")]
+    public float duration = 5f; // 持续时间
+    public GameObject particleOrigin;
+    public ParticleSystem targetParticle;
+
+    void Start()
+    {
+        if (targetParticle != null)
+        {
+            
+        }
+        else
+        {
+            Debug.LogError("哥们你粒子呢？", this);
+        }
+    }
+    //该协程从外部调用，用于播放粒子系统
+    public IEnumerator PlayAndStop()
+    {
+        // 开始播放
+        targetParticle.Play();
+
+        // 等待指定时间
+        yield return new WaitForSeconds(duration);
+
+        // 停止发射新粒子
+        targetParticle.Stop();
+
+        // 等待现有粒子自然消失
+        while (targetParticle.particleCount > 0)
+        {
+            yield return null;
+        }
+
+
+    }
+}
