0281a8604452926d266401394a0d0a50d0df3301
aaaaas811 <3196611630@qq.com>
2025-04-04 17:32:07 +0800

    水滴石穿修复

--- Assets/脚本：as811/水滴石穿/DisAppearController.cs
+++ Assets/脚本：as811/水滴石穿/DisAppearController.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class DisAppearController : MonoBehaviour
 {
+
    public void DisAppear()
     {
-        gameObject.SetActive(false);
+        Destroy(gameObject);
     }
 }
